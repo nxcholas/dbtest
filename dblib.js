39f82f7dc609648407c2f8cf7e48e01e7019d69b
Nicholas Nguyen <nxcholas4652@gmail.com>
2025-04-25 13:42:06 -0700

    asd

---
+++ dblib.js
@@ -0,0 +1,29 @@
+// Add packages
+require("dotenv").config();
+// Add database package and connection string
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 2
+});
+
+const getTotalRecords = () => {
+    const sql = "SELECT COUNT(*) FROM product";
+    return pool.query(sql)
+        .then(result => {
+            return {
+                msg: "success",
+                totRecords: result.rows[0].count
+            }
+        })
+        .catch(err => {
+            return {
+                msg: `Error: ${err.message}`
+            }
+        });
+};
+
+module.exports.getTotalRecords = getTotalRecords;
---
+++ index.js
@@ -0,0 +1,14 @@
+// Add packages
+const dblib = require("./dblib.js");
+
+dblib.getTotalRecords()
+    .then(result => {
+        if (result.msg.substring(0, 5) === "Error") {
+            console.log(`Error Encountered.  ${result.msg}`);
+        } else {
+            console.log(`Total number of database records: ${result.totRecords}`);
+        };
+    })
+    .catch(err => {
+        console.log(`Error: ${err.message}`);
+    });
