c4fdc7601f660aa406a22ca00bae802c0f541b45
Nicholas Nguyen <nxcholas4652@gmail.com>
2025-04-25 13:50:28 -0700

    final tut

--- dblib.js
+++ dblib.js
@@ -26,4 +26,33 @@ const getTotalRecords = () => {
         });
 };
 
+const insertProduct = (product) => {
+  // Will accept either a product array or product object
+  if (product instanceof Array) {
+      params = product;
+  } else {
+      params = Object.values(product);
+  };
+
+  const sql = `INSERT INTO product (prod_id, prod_name, prod_desc, prod_price)
+               VALUES ($1, $2, $3, $4)`;
+
+  return pool.query(sql, params)
+      .then(res => {
+          return {
+              trans: "success", 
+              msg: `Product id ${params[0]} successfully inserted`
+          };
+      })
+      .catch(err => {
+          return {
+              trans: "fail", 
+              msg: `Error on insert of product id ${params[0]}.  ${err.message}`
+          };
+      });
+};
+
+// Add this at the bottom
+module.exports.insertProduct = insertProduct;
+
 module.exports.getTotalRecords = getTotalRecords;
